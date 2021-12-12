13037fde20b12a25d14921fc21cd0794624ec6b3
Victor Benarbia <victor@serpapi.com>
2021-12-12 17:08:03 -0600

    update current version

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/serpapi/google-search-results-golang
 
-go 1.13
+go 1.17
--- model.go
+++ model.go
@@ -18,5 +18,5 @@ type SearchResultArray []interface{}
 
 const (
 	// Current version
-	VERSION = "3.1.0"
+	VERSION = "3.2.0"
 )
